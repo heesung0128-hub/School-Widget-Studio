357404514c82c791169a3fe405b2a30ea3084b05
heesung <heesung0128@gmail.com>
2026-08-25 13:48:50 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/School-Widget-Studio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
